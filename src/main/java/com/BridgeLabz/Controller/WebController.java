2bc23ccc99d7371b03799fa3fd37506d16cd05a4
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-10-10 10:20:41 +0530

    [Saravanan]: Added program to call from parameter

--- src/main/java/com/BridgeLabz/Controller/WebController.java
+++ src/main/java/com/BridgeLabz/Controller/WebController.java
@@ -1,6 +1,7 @@
 package com.BridgeLabz.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,10 @@ public class WebController {
     @GetMapping("/query")
     public String sayHello(@RequestParam String name){
         return "Hello " +name+" from BridgeLabz";
+    }
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        return "Hello "+name+" From BridgeLabz";
     }
 
 }
