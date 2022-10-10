e95a96154e4609ade6e9f89fa2719ba70239b48a
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-10-10 10:10:26 +0530

    [Saravanan]: Added program to call parameter

--- src/main/java/com/BridgeLabz/Controller/WebController.java
+++ src/main/java/com/BridgeLabz/Controller/WebController.java
@@ -1,12 +1,18 @@
 package com.BridgeLabz.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
     @GetMapping("/home")
-    public String home(){
+    public String home() {
         return "Hello from BridgeLabz";
     }
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name){
+        return "Hello " +name+" from BridgeLabz";
+    }
+
 }
